67e46fa38c504338da44a14cfa25d0fb5e5e3c0c
Zenn B <ballosp10@gmail.com>
2025-01-27 13:21:04 +0800

    DataTypesExample

---
+++ DataTypesExample/src/main/java/DataTypesExample.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author Prince
+ */
+public class DataTypesExample {
+
+    public static void main(String[] args) {
+        int age = 21;
+        double height = 5.9;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Prince";
+        
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is a Student? " + isStudent);
+        System.out.println("Name: " + name);
+     }
+}
